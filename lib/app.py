1a5c3645cd3636d0895381a9add70f6894f67559
phil <phil.roth077@gmail.com>
2023-09-07 15:12:36 -0700

    start front with upload window

--- lib/app.py
+++ lib/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, make_response, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 from models import db, Playlist, PlayTrack, Track
 from ipdb import set_trace
+from parser import CSVParser, TxtParser
 from helper import Helper
 
 app = Flask(__name__)
@@ -10,11 +12,33 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///serato.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+CORS(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
+# MAIN UPLOAD
+
+
+@app.route("/upload", methods=["POST"])
+def upload_setlist():
+    # first detect if .txt or csv
+    uploaded_file = request.files["file"]
+    content_type = uploaded_file.content_type.split("/")[1]
+    if content_type == "csv":
+        Helper.center_string_stars("CSV UPLOADER", "+")
+        set_trace()
+    elif content_type == "plain":
+        Helper.center_string_stars("TXT UPLOADER")
+        set_trace()
+    else:
+        Helper.center_string_stars("BAD UPLOADER", "?")
+        return make_response({"error": "Bad"}, 422)
+    set_trace()
+    return make_response({"message": "Succes"})
 
 # TRACKS REST
+
+
 @app.route("/tracks", methods=["GET", "POST"])
 def index_create():
     if request.method == "GET":
@@ -42,4 +66,6 @@ def base():
 
 
 if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+    port = 5555
+    Helper.center_string_stars(f"Server Running on {port}")
+    app.run(port=port, debug=True)
