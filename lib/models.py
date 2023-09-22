57a0f8cc3f1a468496cbcab1e918ffe935b53094
phil <phil.roth077@gmail.com>
2023-09-22 14:36:33 -0700

    start new models

--- lib/models.py
+++ lib/models.py
@@ -30,16 +30,35 @@ class PlayTrack(Base):
         return f"playid: {self.playlist_id} / trackid: {self.track_id}"
 
 
+class ArtistTrack(db.Model):
+    __tablename__ = 'artist_track'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+    track_id = db.Column(db.Integer, db.ForeignKey('tracks.id'))
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self): return f'''id: {self.id}'''
+
+
 class Track(Base):
     __tablename__ = 'tracks'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    bpm = db.Column(db.Float)
+    key = db.Column(db.String)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
 
     # Define the relationship to the Playlist model
     playlists = db.relationship(
         "Playlist", secondary=PlayTrack.__table__, back_populates="tracks")
     play_tracks = db.relationship("PlayTrack", backref=db.backref("track"))
+    artists = db.relationship(
+        "Artist", secondary=ArtistTrack.__table__, backref=db.backref("tracks"))
+    genre = db.relationship("Genre", backref=db.backref("tracks"))
 
     @classmethod
     def create_track_data(cls, session, track, playlist):
@@ -98,3 +117,27 @@ class Playlist(Base):
         db.session.commit()
         for track in setlist.setlist:
             Track.create_track_data(db.session, track, pl)
+
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self): return f'''id: {self.id}'''
+
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self): return f'''id: {self.id}'''
