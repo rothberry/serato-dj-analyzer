77ab6efe64b84c9b176fbb6c4b0c08212949b864
rothberry <phil.roth077@gmail.com>
2025-01-24 18:56:40 -0500

    i make a line bby

--- waves.py
+++ waves.py
@@ -5,6 +5,8 @@ from ipdb import set_trace
 from lib.models import Playlist, Track, PlayTrack
 from sys import argv
 from py_term_helpers import center_string_stars, top_wrap
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -15,7 +17,18 @@ if __name__ == "__main__":
         pl = Playlist.query.all()[0]
         setlist = pl.show_setlist()
 
+        bpms, keys, times = [], [], []
+        # plt_vals = [{'bpm': tr["bpm"], "start": tr["start_time"], "end": tr["end_time"]} for tr in setlist[:3]]
+
+        for tr in setlist:
+            bpms.append(tr["bpm"])
+            keys.append(int(tr["key"][:-1]))
+            times.append(tr["start_time"])
+
+        plt.plot(times, bpms, times, keys)
         
+        plt.show()
+
         set_trace()
 
         center_string_stars("DONE")
